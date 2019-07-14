d59ec6d161a29bf7233b06031ffc4867c21d53a2
jsleetw <ail529@gmail.com>
2019-07-15 03:32:22 +0800

    valid-parentheses

---
+++ 20.valid-parentheses.243318899.ac.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        map = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "[",
+        }
+        #use stack
+        stack = []
+        
+        for char in s:
+            if char in map:
+                if stack:
+                    top_element = stack.pop() 
+                else:
+                    top_element = "#"
+                
+                if top_element != map[char]:
+                    return False
+            else:
+                stack.append(char)
+        
+        if not stack:
+            return True
