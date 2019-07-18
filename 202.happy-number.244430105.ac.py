b208162506e784d93a228d4d6c6103280e8d4bf2
jsleetw <ail529@gmail.com>
2019-07-19 04:27:52 +0800

    Happy Number

---
+++ 202.happy-number.244430105.ac.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        mem = set()
+        
+        while n != 1:
+            sum = 0
+            for i in str(n):
+                sum += int(i)*int(i)
+            n = sum
+
+            if n in mem:
+                return False
+            else:
+                mem.add(n)
+        else:
+            return True
